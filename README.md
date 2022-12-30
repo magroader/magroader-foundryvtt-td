b69d0f7b99b0d47c0f12146b78ce687e64028ce5
Colin C <magroader@gmail.com>
2022-12-30 16:45:29 -0500

    First commit, barebones mod

---
+++ README.md
@@ -0,0 +1,3 @@
+To install, use this module path:
+
+https://github.com/magroader/magroader-foundryvtt-td/releases/latest/download/module.json
---
+++ scripts/main.mjs
@@ -0,0 +1,7 @@
+//import { Something } from './something.mjs';
+
+//let something = new Something();
+
+Hooks.once('init', function () {
+  //something.init();
+});
